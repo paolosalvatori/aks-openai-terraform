ecafb17e8bb4e1709147220e60eb4a7eb52fd04b
Paolo Salvatori <paolos@microsoft.com>
2024-02-15 12:11:27 +0100

    Fixed taints and labels

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -108,73 +108,33 @@ resource "azurerm_monitor_diagnostic_setting" "settings" {
 
   enabled_log {
     category = "kube-apiserver"
-
-    retention_policy {
-      enabled = true  
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "kube-audit"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "kube-audit-admin"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "kube-controller-manager"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "kube-scheduler"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "cluster-autoscaler"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   enabled_log {
     category = "guard"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 
   metric {
     category = "AllMetrics"
-
-    retention_policy {
-      enabled = true
-      days    = var.log_analytics_retention_days
-    }
   }
 }
